4920d7b0e121e0fa89ca09bb798ba284a7a487ba
Edward Opare-Aryee <ed@thisissoon.com>
2017-07-27 11:06:01 +0100

    chore(demo): added example code for directive onInViewportChange event

--- src/demo/app/app.component.ts
+++ src/demo/app/app.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+  highlight = false;
+
+  onInViewportChange(inViewport: boolean) {
+    this.highlight = inViewport;
+  }
+}

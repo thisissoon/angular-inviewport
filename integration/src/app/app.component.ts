65bd0995a82bd9f935fd5cc1787a96f76f04f7ea
Edward Opare-Aryee <ed@thisissoon.com>
2017-07-27 11:06:48 +0100

    test(inViewport): added e2e tests

--- integration/src/app/app.component.ts
+++ integration/src/app/app.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+  highlight = true;
+
+  onInViewportChange(inViewport: boolean) {
+    this.highlight = inViewport;
+  }
+}

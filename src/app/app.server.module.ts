30e6a570bf8858f10fa2cc0c711f1e94487ebfa9
Mike Behnke <mike.behnke@kumanu.com>
2020-07-24 14:39:19 -0400

    fix(@nguniversal/module-map-ngfactory-loader): remove module-map-ngfactory-loader (deprecated)

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
-import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 import { InViewportModule } from './in-viewport';
 
 @NgModule({
-  imports: [
-    AppModule,
-    ServerModule,
-    InViewportModule.forServer(),
-    ModuleMapLoaderModule
-  ],
-  bootstrap: [AppComponent]
+  imports: [AppModule, ServerModule, InViewportModule.forServer()],
+  bootstrap: [AppComponent],
 })
 export class AppServerModule {}
